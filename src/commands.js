45b10da89ebdfd2060772c2c6561749d3bf8cd78
Joey Zhao <joey@southbanksoftware.com>
2018-07-06 15:14:08 +1000

    open text editor

--- src/commands.js
+++ src/commands.js
@@ -1,12 +1,22 @@
 const vscode = require('vscode');
-const {getMongoInspector} = require('./connection');
+const { getMongoInspector } = require('./connection');
 
 const registerCommands = () => {
     vscode.commands.registerCommand('mongoRunner.serverStatus', () => {
         console.log('serverStatus');
         const inspector = getMongoInspector();
         inspector.serverStats()
-        .then(stats => console.log(stats));
+            .then(stats => {
+                return vscode.workspace.openTextDocument({ content: JSON.stringify(stats), language: 'json' });
+            })
+            .then((document) => {
+                console.log('document:', document);
+                return vscode.window.showTextDocument(document, 1, true);
+            })
+            .then((doc) => {
+                vscode.commands.executeCommand('vscode.executeFormatDocumentProvider', doc.document.uri)
+            })
+            .catch(err => console.error(err));
     });
 };
 
