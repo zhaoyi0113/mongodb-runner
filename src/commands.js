4c51051544eccb5b69bc8472dec046bbdbd56108
Joey Zhao <joey@southbanksoftware.com>
2018-07-06 15:19:29 +1000

    add server build info command

--- src/commands.js
+++ src/commands.js
@@ -1,23 +1,35 @@
 const vscode = require('vscode');
 const { getMongoInspector } = require('./connection');
 
+const openTextInEditor = (text, language='json') => {
+    return vscode.workspace.openTextDocument({ content: text, language })
+    .then((doc) => vscode.window.showTextDocument(doc, 1, true));
+}
+
+const serverStatusHandler = () => {
+    const inspector = getMongoInspector();
+    inspector.serverStats()
+        .then(stats => {
+            return openTextInEditor(JSON.stringify(stats));
+        })
+        // .then((doc) => {
+        //     vscode.commands.executeCommand('vscode.executeFormatDocumentProvider', doc.document.uri)
+        // })
+        .catch(err => console.error(err));
+};
+
+const serverBuildInfoHandler = () => {
+    const inspector = getMongoInspector();
+    inspector.buildInfo()
+        .then(stats => {
+            return openTextInEditor(JSON.stringify(stats));
+        })
+        .catch(err => console.error(err));
+};
+
 const registerCommands = () => {
-    vscode.commands.registerCommand('mongoRunner.serverStatus', () => {
-        console.log('serverStatus');
-        const inspector = getMongoInspector();
-        inspector.serverStats()
-            .then(stats => {
-                return vscode.workspace.openTextDocument({ content: JSON.stringify(stats), language: 'json' });
-            })
-            .then((document) => {
-                console.log('document:', document);
-                return vscode.window.showTextDocument(document, 1, true);
-            })
-            .then((doc) => {
-                vscode.commands.executeCommand('vscode.executeFormatDocumentProvider', doc.document.uri)
-            })
-            .catch(err => console.error(err));
-    });
+    vscode.commands.registerCommand('mongoRunner.serverStatus', serverStatusHandler);
+    vscode.commands.registerCommand('mongoRunner.serverBuildInfo', serverBuildInfoHandler);
 };
 
 module.exports = {
