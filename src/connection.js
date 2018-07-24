d4ec65a366b45e13a65f752fd96255ada8efa88f
Joey Zhao <zhaoyi0113@gmail.com>
2018-07-24 14:51:57 +1000

    create mongodb connection in the plugin

--- src/connection.js
+++ src/connection.js
@@ -1,22 +1,30 @@
-const mongodbTree = require('mongodb-topology');
-const vscode = require('vscode');
+const { TreeInspector } = require("mongodb-topology");
+const { MongoClient } = require("mongodb");
+
+const vscode = require("vscode");
 
 let inspector;
 
-const connectMongoDB = (mongoConfig) => {
-    // connect to mongodb instance
-    return mongodbTree
-        .connect(mongoConfig.url, {})
-        .then((d) => {
-            inspector = d;
-            return inspector.inspect();
-        })
-        .catch((err) => {
-            console.error(err);
-            vscode.window.showErrorMessage('Failed to connect MongoDB.');
-        })
+const connectMongoDB = mongoConfig => {
+  // connect to mongodb instance
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(
+      mongoConfig.url, {useNewUrlParser: true}, 
+      (err, driver) => {
+        if (err) {
+          console.error(err);
+          vscode.window.showErrorMessage("Failed to connect MongoDB.");
+          return reject(err);
+        }
+        inspector = new TreeInspector(driver);
+        inspector.inspect().then(tree => {
+          resolve(tree);
+        }).catch(err => reject(err));
+      }
+    );
+  });
 };
 
 const getMongoInspector = () => inspector;
 
-module.exports = {connectMongoDB, getMongoInspector};
+module.exports = { connectMongoDB, getMongoInspector };
